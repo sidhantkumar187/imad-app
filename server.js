121958cd0f3b791c489c569d6732deb13e8be57d
sidhantkumar187 <30744292+sidhantkumar187@users.noreply.github.com>
2018-03-02 23:09:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,21 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {          //for handling specific url
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get("/article-one", function (req,res) {
+    res.send("Article one requested and will be served here");
+});
+
+app.get("/article-two", function (req,res) {
+    res.send("Article two requested and will be served here");
+});
+
+app.get("/article-three", function (req,res) {
+    res.send("Article three requested and will be served here");
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
