28aab8a40e445aa1b1a02ff08e2f7defe9a70de8
sidhantkumar187 <30744292+sidhantkumar187@users.noreply.github.com>
2018-03-10 20:59:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,10 @@ element.innerHTML = 'Hola-Main.js';
 // moving the image
 
 var img = document.getElementById('madi');
+var marginleft = 0;
+function moveRight (){
+    img.style.marginleft = marginleft + 'px';
+}
 img.onclick = function (){
     var interval = setInterval(moveRight, 100);
 };
