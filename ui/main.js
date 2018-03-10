bb21659310d160d1185fd286484a4642151b032a
sidhantkumar187 <30744292+sidhantkumar187@users.noreply.github.com>
2018-03-10 20:49:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,12 @@
 console.log('Loaded!');
 
+// changing the text of main-text
 var element = document.getElementById('main-text');
 element.innerHTML = 'Hola-Main.js';
+
+// moving the image
+
+var img = document.getElementById('madi');
+img.onclick = function (){
+    img.style.marginleft = '110px';
+}
