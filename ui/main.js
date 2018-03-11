652134ab934e01ce93d82427eb74c35f945ce34f
sidhantkumar187 <30744292+sidhantkumar187@users.noreply.github.com>
2018-03-11 23:03:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,24 @@
 
-// this is client-side java-script 
+// // this is client-side java-script 
 
 
-console.log('Loaded!');
+// console.log('Loaded!');
 
-// changing the text of main-text
-var element = document.getElementById('main-text');
-element.innerHTML = 'Hola-Main.js';
+// // changing the text of main-text
+// var element = document.getElementById('main-text');
+// element.innerHTML = 'Hola-Main.js';
 
-// moving the image
+// // moving the image
 
-var img = document.getElementById('madi');
-var marginLeft = 0;
-function moveRight (){
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function (){
-    var interval = setInterval(moveRight, 50);  // calling moveRight function to move image by passing time interval(50ms)
-};
+// var img = document.getElementById('madi');
+// var marginLeft = 0;
+// function moveRight (){
+//     marginLeft = marginLeft + 1;
+//     img.style.marginLeft = marginLeft + 'px';
+// }
+// img.onclick = function (){
+//     var interval = setInterval(moveRight, 50);  // calling moveRight function to move image by passing time interval(50ms)
+// };
 //---------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------
 
