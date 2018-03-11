2a4efd0e89ca90e18c00057d1601ab9572d93eaf
sidhantkumar187 <30744292+sidhantkumar187@users.noreply.github.com>
2018-03-11 22:56:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ button.onclick = function (){
             if (request.status === 200) {
                 var counter = request.responseText;
                 var span = document.getElementById('count');
-                span.HTML = counter.toString();
+                span.innerHTML = counter.toString();
             }
         }
     //Not DONE yet
